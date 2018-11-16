706e4289dcdba60374b5fe8ce8d2102aa5f957bf
g-gaudin <34863251+g-gaudin@users.noreply.github.com>
2018-11-16 16:52:46 +0100

    Add files via upload

--- GildedRose.java
+++ GildedRose.java
@@ -1,6 +1,10 @@
 package gildedrose;
 
 class GildedRose {
+
+    private static final String CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -28,46 +32,38 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                    && !items[i].name.equals(CONCERT)) {
+                if (isNotSpoiled(i)) {
+                    items[i].quality = items[i].quality - 1;
                 }
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    if (items[i].name.equals(CONCERT)) {
+                        if ((items[i].sellIn < 11) && (items[i].quality < 50)) {
+                            items[i].quality = items[i].quality + 1;
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if ((items[i].sellIn < 6) && (items[i].quality < 50)) {
                                 items[i].quality = items[i].quality + 1;
-                            }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals(SULFURAS)) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                    if (!items[i].name.equals(CONCERT)) {
+                        if (isNotSpoiled(i)) {
+                            items[i].quality = items[i].quality - 1;
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        items[i].quality = 0;
                     }
                 } else {
                     if (items[i].quality < 50) {
@@ -76,5 +72,9 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private boolean isNotSpoiled(int i){
+        return ((items[i].quality > 0) && (!items[i].name.equals(SULFURAS)));
     }
 }
--- GildedRoseTest.java
+++ GildedRoseTest.java
@@ -1,9 +1,13 @@
 package gildedrose;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseTest {
+
+    private static final String AGEDBRIE = "Aged Brie";
+    private static final String CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 
     @Test
     public static void foo() {
@@ -17,14 +21,14 @@ public class GildedRoseTest {
         assertEquals(appTest,appTemoin);
 
         // Les items "Aged Brie" gagnent en qualité avec le temps
-        appTest = new GildedRose(new Item[]{new Item("Aged Brie",10,20)});
-        appTemoin = new GildedRose(new Item[]{new Item("Aged Brie",9,21)});
+        appTest = new GildedRose(new Item[]{new Item(AGEDBRIE,10,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(AGEDBRIE,9,21)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // Les items "Aged Brie" gagnent en qualité deux fois plus vite si la date de péremption est passée
-        appTest = new GildedRose(new Item[]{new Item("Aged Brie",0,20)});
-        appTemoin = new GildedRose(new Item[]{new Item("Aged Brie",-1,22)});
+        appTest = new GildedRose(new Item[]{new Item(AGEDBRIE,0,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(AGEDBRIE,-1,22)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
@@ -35,38 +39,38 @@ public class GildedRoseTest {
         assertEquals(appTest,appTemoin);
 
         // Si la qualité d'un item est de 50, elle ne peut pas augmenter au-delà
-        appTest = new GildedRose(new Item[]{new Item("Aged Brie",10,50)});
-        appTemoin = new GildedRose(new Item[]{new Item("Aged Brie",9,50)});
+        appTest = new GildedRose(new Item[]{new Item(AGEDBRIE,10,50)});
+        appTemoin = new GildedRose(new Item[]{new Item(AGEDBRIE,9,50)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // Les items "Backstage passes..." gagnent en qualité avec le temps : un point chaque jour s'il en reste au moins 11 avant péremption
-        appTest = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",40,20)});
-        appTemoin = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",39,21)});
+        appTest = new GildedRose(new Item[]{new Item(CONCERT,40,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,39,21)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // Les items "Backstage passes..." gagnent en qualité avec le temps : deux points chaque jour s'il en reste entre 6 et 10 avant péremption
-        appTest = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",8,20)});
-        appTemoin = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",7,22)});
+        appTest = new GildedRose(new Item[]{new Item(CONCERT,8,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,7,22)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // Les items "Backstage passes..." gagnent en qualité avec le temps : trois points chaque jour s'il en 5 ou moins avant péremption
-        appTest = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",4,20)});
-        appTemoin = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",3,23)});
+        appTest = new GildedRose(new Item[]{new Item(CONCERT,4,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,3,23)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // Les items "Backstage passes..." voient leurs qualités instantanément passer à 0 dès lors que la date de péremption est atteinte
-        appTest = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",0,20)});
-        appTemoin = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",-1,0)});
+        appTest = new GildedRose(new Item[]{new Item(CONCERT,0,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,-1,0)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // La qualité des items "Backstage passes..." est maintenue à 0 après que la date de péremption est passée
-        appTest = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",-5,0)});
-        appTemoin = new GildedRose(new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",-6,0)});
+        appTest = new GildedRose(new Item[]{new Item(CONCERT,-5,0)});
+        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,-6,0)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
