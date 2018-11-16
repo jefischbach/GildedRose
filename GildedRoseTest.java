1d2039015ef7442408fd321b7d153879b663050a
g-gaudin <34863251+g-gaudin@users.noreply.github.com>
2018-11-16 18:39:22 +0100

    Add files via upload

--- GildedRoseTest.java
+++ GildedRoseTest.java
@@ -8,8 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseTest {
 
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     private static final String AGEDBRIE = "Aged Brie";
-    private static final String CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 
     private GildedRoseTest(){}
 
@@ -36,8 +37,8 @@ public class GildedRoseTest {
         assertEquals(appTest,appTemoin);
 
         // Les items "Sulfuras, Hand of Ragnaros" ne perdent jamais en qualité et la date de péremption ne bouge pas
-        appTest = new GildedRose(new Item[]{new Item("Sulfuras, Hand of Ragnaros",10,80)});
-        appTemoin = new GildedRose(new Item[]{new Item("Sulfuras, Hand of Ragnaros",10,80)});
+        appTest = new GildedRose(new Item[]{new Item(SULFURAS,10,80)});
+        appTemoin = new GildedRose(new Item[]{new Item(SULFURAS,10,80)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
@@ -48,32 +49,32 @@ public class GildedRoseTest {
         assertEquals(appTest,appTemoin);
 
         // Les items "Backstage passes..." gagnent en qualité avec le temps : un point chaque jour s'il en reste au moins 11 avant péremption
-        appTest = new GildedRose(new Item[]{new Item(CONCERT,40,20)});
-        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,39,21)});
+        appTest = new GildedRose(new Item[]{new Item(BACKSTAGE,40,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(BACKSTAGE,39,21)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // Les items "Backstage passes..." gagnent en qualité avec le temps : deux points chaque jour s'il en reste entre 6 et 10 avant péremption
-        appTest = new GildedRose(new Item[]{new Item(CONCERT,8,20)});
-        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,7,22)});
+        appTest = new GildedRose(new Item[]{new Item(BACKSTAGE,8,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(BACKSTAGE,7,22)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // Les items "Backstage passes..." gagnent en qualité avec le temps : trois points chaque jour s'il en 5 ou moins avant péremption
-        appTest = new GildedRose(new Item[]{new Item(CONCERT,4,20)});
-        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,3,23)});
+        appTest = new GildedRose(new Item[]{new Item(BACKSTAGE,4,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(BACKSTAGE,3,23)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // Les items "Backstage passes..." voient leurs qualités instantanément passer à 0 dès lors que la date de péremption est atteinte
-        appTest = new GildedRose(new Item[]{new Item(CONCERT,0,20)});
-        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,-1,0)});
+        appTest = new GildedRose(new Item[]{new Item(BACKSTAGE,0,20)});
+        appTemoin = new GildedRose(new Item[]{new Item(BACKSTAGE,-1,0)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
         // La qualité des items "Backstage passes..." est maintenue à 0 après que la date de péremption est passée
-        appTest = new GildedRose(new Item[]{new Item(CONCERT,-5,0)});
-        appTemoin = new GildedRose(new Item[]{new Item(CONCERT,-6,0)});
+        appTest = new GildedRose(new Item[]{new Item(BACKSTAGE,-5,0)});
+        appTemoin = new GildedRose(new Item[]{new Item(BACKSTAGE,-6,0)});
         appTest.updateQuality();
         assertEquals(appTest,appTemoin);
 
@@ -81,7 +82,7 @@ public class GildedRoseTest {
         appTest = new GildedRose(new Item[]{new Item("Conjured Test",40,20)});
         appTemoin = new GildedRose(new Item[]{new Item("Conjured Test",39,18)});
         appTest.updateQuality();
-        //assertEquals(appTest,appTemoin);
+        assertEquals(appTest,appTemoin);
     }
 
 }
